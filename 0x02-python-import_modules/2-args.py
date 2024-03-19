d4e31a0d4383f172c79a0fb7df147add8d34256d
madox111803 <madox111803@gmail.com>
2024-03-19 10:43:59 -0700

    py is fun

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-    i = len(sys.argv) - 1
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else :
-        print("{} arguments:".format(i))
-    for num in range(i):
-        print("{}: {}".format(num + 1, sys.argv[num + 1]))
 
+    # Count the number of arguments passed (excluding the script name itself)
+    count = len(sys.argv) - 1
+
+    # Check the count of arguments and print the appropriate message
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    # Iterate over the arguments and print them with their respective positions
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
