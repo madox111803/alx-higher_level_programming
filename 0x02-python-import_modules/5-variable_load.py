73c7eceacc2b8f3612e3ac7abea7235c8d912feb
madox111803 <madox111803@gmail.com>
2024-03-19 10:27:55 -0700

    py is fun

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """
-    Print the value of the variable 'a' imported from the 'variable_load_5' module.
-    """
+    """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
+
     print(a)
