37d84f1c5bda443a28df48e0925d1718cc9b5252
madox111803 <madox111803@gmail.com>
2024-03-19 10:40:47 -0700

    py is fun

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = len(sys, argv) - 1
+    i = len(sys.argv) - 1
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
         print("{} argument:".format(i))
     else :
         print("{} arguments:".format(i))
-    for num in range(1, i):
-        print("{}: {}".format(i, sys.argv[i]))
+    for num in range(i):
+        print("{}: {}".format(num + 1, sys.argv[num + 1]))
 
