9d5dd325304639ac689f138d2ad613e1c6961894
madox111803 <madox111803@gmail.com>
2024-03-18 23:48:53 -0700

    py is fun

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,8 @@
 def divisible_by_2(my_list=[]):
     a = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             a.append(True)
         else:
             a.append(False)
     return a
-
