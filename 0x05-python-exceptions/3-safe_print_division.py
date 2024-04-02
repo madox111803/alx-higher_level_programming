cca08a1fa60e9ba0425359298f199e28144aee16
madox111803 <madox111803@gmail.com>
2024-04-02 00:17:11 -0700

    py is fun

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, TypeError):
+        div = None
+    finally:
+        print("Inside result:{}".format(div))
+    return (div)
