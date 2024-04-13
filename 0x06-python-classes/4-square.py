d90ae9702b81ad1eb00138f3e868d6f9d887345e
madox111803 <madox111803@gmail.com>
2024-04-13 08:54:03 -0700

    oop

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""Area of a square"""
+!/usr/bin/python3
+"""Access and update private attribute"""
 
 
 class Square:
@@ -7,24 +7,24 @@ class Square:
     Instantiation with area method """
 
     def __init__(self, size=0):
-        """init attribute size"""
+        """Initializes attribute size """
         self.__size = size
-    
-    @property
-    """getter/setter for square"""
-    def size(self):
-        return self.__size
 
-    @size.setter
-    def size(self, value):
-        """init attribute size"""
-        if type(value) is not int:
-            raise TypeError("size must be an interger")
-        elif value < 0:
-            raise ValueError("size must be an interger")
-        else:
-            self.__size = value
     def area(self):
         """Calculate area of square"""
         return (self.__size * self.__size)
 
+    @property
+    def size(self):
+        """Setter for square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
