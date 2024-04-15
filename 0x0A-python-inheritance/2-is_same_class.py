565ad74ca616574d495c417a522472473091312e
madox111803 <madox111803@gmail.com>
2024-04-15 05:30:54 -0700

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 """Module defining function to test class membership"""
 def is_same_class(obj, a_class):
     """Returns True if object is an instance and not a subclass of a_class"""
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
