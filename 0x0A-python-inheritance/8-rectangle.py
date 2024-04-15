468b0771ae18dcc617119ca59bd7c3827ba38067
madox111803 <madox111803@gmail.com>
2024-04-15 12:01:17 -0700

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,7 +2,11 @@
 """Module Rectangle
 Creates a Rectangle class
 """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Represents a rectangle
     Private instance attributes:
@@ -10,13 +14,15 @@ class Rectangle(BaseGeometry):
         - height
     Inherits from BaseGeometry
     """
+
     def __init__(self, width, height):
         """Initializes an instance
 
         Args:
-                - width: rectangle width
-                - height: rectangle height
+            - width: rectangle width
+            - height: rectangle height
         """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
