b920cef2b485a8eba17368f4adf06917ff6334e4
madox111803 <madox111803@gmail.com>
2024-04-16 22:33:28 +0000

    i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" this module defines a text file-reading function"""
+"""This module defines a text file-reading function"""
 
 
 def write_file(filename="", text=""):
-	"""write a stringe to a UTF8 text file"""
-	with open(filename, "w", encoding="utf-8") as f:
-		return f.write(text) 
+    """Writes a string to a UTF8 text file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
