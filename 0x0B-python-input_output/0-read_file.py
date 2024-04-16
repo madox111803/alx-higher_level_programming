871acc0cf95509322fb986a0f48664c6c9676209
madox111803 <madox111803@gmail.com>
2024-04-16 22:17:06 +0000

    i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-	"""function that reads a text file UTF8 and prints it to stdout"""
+	"""prints the content of a UTF8 text file"""
 	with open(filename, encoding="utf-8") as f:
 		print(f.read(), end='')
