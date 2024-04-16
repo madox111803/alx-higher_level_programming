c65f7d495dd846236a6bd290c73afbda1ee00019
madox111803 <madox111803@gmail.com>
2024-04-16 22:55:45 +0000

    i/o

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""this module defines a string-to-JSON function"""
+"""Defines a string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
-	"""Returns the JSON representation of an object (string)"""
-	return json.dumps(my_obj)
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
