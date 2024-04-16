b133480127a780628ad608726d02e07055c07921
madox111803 <madox111803@gmail.com>
2024-04-16 22:30:38 +0000

    i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-	"""write a stringe to a UTF8 text file and returns the number of characters written"""
+	"""write a stringe to a UTF8 text file"""
 	with open(filename, "w", encoding="utf-8") as f:
 		return f.write(text) 
