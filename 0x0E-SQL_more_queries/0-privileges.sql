fa91413b49b04fb6cf700d5e6d646f6174f5ed81
madox111803 <madox111803@gmail.com>
2024-05-23 14:44:34 -0700

    done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 GRANT *
 ON *.*
-TO user_0d_1@localhost, user_0d_2@localhost;
+TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
