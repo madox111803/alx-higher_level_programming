698d7233aeb4bed29f8afc4426b017944f38cad0
madox111803 <madox111803@gmail.com>
2024-05-23 16:31:32 -0700

    done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
-	ADD FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
+	ADD FOREIGN KEY(state_id)
+	REFERENCES states(id)
 );
