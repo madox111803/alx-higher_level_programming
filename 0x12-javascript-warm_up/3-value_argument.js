8898f6d3864e1ae08f6c8996f29398eb01669136
madox111803 <madox111803@gmail.com>
2024-07-10 05:49:10 -0700

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 let msg = '';
 if (process.argv[2] === undefined) {
-  mssg = 'No argument';
+  msg = 'No argument';
 } else {
-  mss process.argv[2];
+  msg = process.argv[2];
 }
-console.log(mssg);
+console.log(msg);
